bd37a672eafb23d0138a3dec6d52894c565f6cf3
Gabriel-Philot <bilphilot@gmail.com>
2024-06-09 15:00:14 -0300

    adjust_monitoring

--- src/monitoring/src/main.py
+++ src/monitoring/src/main.py
@@ -5,37 +5,10 @@ from resources.bucket_files import download_file_to_dataframe, aws_bucket, path_
 from resources.process_tools import process_dataframe, display_results, create_expander_with_checkboxes
 from resources.visual_tools import create_line_chart, create_success_card
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="centered")
+path_image = "resources/docker_monitoring.png"
+st.image(path_image, width=600)
 
-# Custom HTML/JS for the banner
-custom_js = """
-<script>
-// Modify the decoration on top to reuse as a banner
-
-// Locate elements
-var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
-var sidebar = window.parent.document.querySelectorAll('[data-testid="stSidebar"]')[0];
-
-// Observe sidebar size
-function outputsize() {
-    decoration.style.left = `${sidebar.offsetWidth}px`;
-}
-
-new ResizeObserver(outputsize).observe(sidebar);
-
-// Adjust sizes
-outputsize();
-decoration.style.height = "3.0rem";
-decoration.style.right = "45px";
-
-// Adjust image decorations
-decoration.style.backgroundImage = "url(resources/docker_monitoring.png)";
-decoration.style.backgroundSize = "contain";
-</script>        
-"""
-
-# Display the custom JS
-st.components.v1.html(custom_js, width=0, height=0)
 
 
 # Baixar os arquivos e armazenar no DataFrame
@@ -43,7 +16,7 @@ df_lambda = download_file_to_dataframe(aws_bucket, path_lambda)
 df_glue = download_file_to_dataframe(aws_bucket, path_glue)
 
 
-st.title("🔧 My Own loggin monitor tool")
+st.title("🔧 My own loggin monitor tool")
 
 # Adiciona conteúdo na barra lateral
 st.sidebar.title('Last runs')
